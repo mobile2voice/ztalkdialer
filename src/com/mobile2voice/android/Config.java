49454fc9dc4f710a040f0063a145677a291a8c26
Mobile2Voice <mobile2voice@gmail.com>
2014-03-22 20:18:08 +0600

    Update Config.java

--- src/com/mobile2voice/android/Config.java
+++ src/com/mobile2voice/android/Config.java
@@ -4,12 +4,12 @@ public interface Config {
 	
 	//used to share GCM regId with application server
 	
-	//static final String APP_SERVER_URL = "http://64.20.56.232/gcm/gcm.php?shareRegId=1";
+	//static final String APP_SERVER_URL = "http://64.20.56.232/~ztalk/gcm/gcm.php?shareRegId=1";
 	
-	static final String APP_SERVER_URL = "http://64.20.56.232:8080/GCM-App-Server/GCMNotification?shareRegId=1";	
+	static final String APP_SERVER_URL = "http://64.20.56.232:8080/~ztalk/GCM-App-Server/GCMNotification?shareRegId=1";	
 
 	// Google Project Number
-	static final String GOOGLE_PROJECT_ID = "6276034927";
+	static final String GOOGLE_PROJECT_ID = "1042290160151";
 	
 	static final String SHOW_MESSAGE = "com.mobile2voice.android.DISPLAY_MESSAGE";	
 	static final String EXTRA_MESSAGE = "message";
